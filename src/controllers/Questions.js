4c764a687eee613315b29893f74454aa392df1c3
klauza <klauza.uk@gmail.com>
2019-07-18 12:07:20 +0100

    refactor, pref before next question

--- src/controllers/Questions.js
+++ src/controllers/Questions.js
@@ -20,126 +20,131 @@ const QuestionsCtrl = (function(){
 
     showQuestion: function(level){
       
+      // display block with questions
+      let allAnswers = document.querySelector('.answers');
+      allAnswers.style.display = "flex";
+      let answers = Array.from(allAnswers.children);
+
+      // get chosen actor's data
+      let actor = PersonCtrl.getPerson();
+      
+    
+
+
       switch(level){
         case 1:
           console.log('hello level 1');
+          // sum of actor movies
+          let actorMoviesAmount = actor.movies.length;  
+          let randomActorMovieId = Math.floor(Math.random() * actorMoviesAmount); 
+          let actorMovie = actor.movies[randomActorMovieId];
+          question.cookie_1_movie = actorMovie;
+
+          // hardcoded wrong answers
+          let randomMovie1 = ['Fight Club', 'The Green Mile', 'The Matrix', 'Intouchables', 'Pirates of the Caribbean', 'Cast Away', 'The Butterfly Effect'];
+          let randomMovie2 = ['Léon', 'Gladiator', 'The Hangover', 'Se7en', 'Seven Pounds', 'A Beautiful Mind', 'The Da Vinci Code', 'Gran Torino'];
+          let randomNumber1 = Math.random() * randomMovie1.length;  // 0 1 2 => length 3
+          let random1 = Math.floor(randomNumber1); 
+          let randomNumber2 = Math.random() * randomMovie2.length;  // 0 1 2
+          let random2 = Math.floor(randomNumber2); 
+
+          // put all, wrong and a correct answers in new array
+          let answerArray = new Array; // init new array with answers to be outputted later
+          answerArray.push(actorMovie, randomMovie1[random1], randomMovie2[random2]);
+
+          function getRandomAnswer(){
+            let randomNum = Math.random() * answerArray.length;  // if length==3, will be [0 1 2]
+            let random = Math.floor(randomNum);
+            return random;
+          }
+    
+          // FILLING BLOCKS WITH ANSWERS
+          fillAnswers();
+
+          function fillAnswers(){
+            if(answers[0].textContent==''){
+            
+              const randomAnswerId = getRandomAnswer();   // variable for id of randomized answer
+    
+              // put random answer into first block
+              answers[0].textContent=answerArray[randomAnswerId];
+    
+              // delete answer with this id from answerArray
+              answerArray.splice(randomAnswerId, 1);   
+              
+              fillAnswers();   // run the whole function again!
+    
+            } else if(answers[1].textContent==''){
+              const randomAnswerId = getRandomAnswer();
+              answers[1].textContent=answerArray[randomAnswerId];
+              answerArray.splice(randomAnswerId, 1);  
+              fillAnswers();
+    
+            } else if(answers[2].textContent==''){
+              const randomAnswerId = getRandomAnswer();
+              answers[2].textContent=answerArray[randomAnswerId];
+              answerArray.splice(randomAnswerId, 1);  
+              fillAnswers();
+    
+            } else {
+              return
+            }
+          }
+
           break;
+
+
         case 2:
           console.log('hello level 2');
           break;
+          
+
         case 3:
           console.log('hello level 3');
           break
         
       }
 
-      // display block with questions
-      let allAnswers = document.querySelector('.answers');
-      allAnswers.style.display = "flex";
-      let answers = Array.from(allAnswers.children);
-
-
-      // get chosen actor's data
-      let actor = PersonCtrl.getPerson();
-      let actorMoviesAmount = actor.movies.length;   // sum of actor movies
-
-      let randomActorMovieId = Math.floor(Math.random() * actorMoviesAmount); 
-      let actorMovie = actor.movies[randomActorMovieId];
-      question.cookie_1_movie = actorMovie;
-
-     
-      // hardcoded wrong answers
-      let randomMovie1 = ['Fight Club', 'The Green Mile', 'The Matrix', 'Intouchables', 'Pirates of the Caribbean', 'Cast Away', 'The Butterfly Effect'];
-      let randomMovie2 = ['Léon', 'Gladiator', 'The Hangover', 'Se7en', 'Seven Pounds', 'A Beautiful Mind', 'The Da Vinci Code', 'Gran Torino'];
-
-      let randomNumber1 = Math.random() * randomMovie1.length;  // 0 1 2 => length 3
-      let random1 = Math.floor(randomNumber1); 
-      let randomNumber2 = Math.random() * randomMovie2.length;  // 0 1 2
-      let random2 = Math.floor(randomNumber2); 
-
-      // put all answers in new array
-      let answerArray = new Array; // init new array with answers to be outputted later
-      answerArray.push(actorMovie, randomMovie1[random1], randomMovie2[random2]);
-
-      // put random answer into random place
-      function getRandomAnswer(){
-        let randomNum = Math.random() * answerArray.length;  // if length==3, will be [0 1 2]
-        let random = Math.floor(randomNum);
-        return random;
-      }
-
-      // FILLING BLOCKS WITH ANSWERS
-      fillAnswers();
-      function fillAnswers(){
-        if(answers[0].textContent==''){
-        
-          const randomAnswerId = getRandomAnswer();   // variable for id of randomized answer
-
-          // put random answer into first block
-          answers[0].textContent=answerArray[randomAnswerId];
-
-          // delete answer with this id from answerArray
-          answerArray.splice(randomAnswerId, 1);   
-          //console.log('the new array is:', answerArray);
-          
-
-          fillAnswers();   // run the whole function again!
-
-        } else if(answers[1].textContent==''){
-          const randomAnswerId = getRandomAnswer();
-          answers[1].textContent=answerArray[randomAnswerId];
-          answerArray.splice(randomAnswerId, 1);  
-          fillAnswers();
-
-        } else if(answers[2].textContent==''){
-          const randomAnswerId = getRandomAnswer();
-          answers[2].textContent=answerArray[randomAnswerId];
-          answerArray.splice(randomAnswerId, 1);  
-          fillAnswers();
-
-        } else {
-          return
-        }
-      }
       
-
       // GUESSING THE ANSWER
       answers.forEach((button)=>{
-        button.addEventListener('click', tryGuess);
+        button.addEventListener('click', QuestionsCtrl.tryGuess);
       })
+      
 
-      function tryGuess(){
-        let guess = this.textContent;   // string of clicked answer
-        let correctAnswer = actorMovie;   // string of correct answer
 
-        if (correctAnswer == guess){ 
-          console.log('correct!');
+    },
 
-          QuestionsCtrl.clearAnswers();  // empty string of answers
-          QuestionsCtrl.hideAnswers();   // hide block 'answers'
+    tryGuess: function(){
+      let guess = this.textContent;   // string of clicked answer
+      let correctAnswer = question.cookie_1_movie;   // string of correct answer
 
-          LevelCtrl.IncreaseScoreByOne(); // incement score 
+      if (correctAnswer == guess){ 
+        console.log('correct!');
 
-          // remove event listeners from buttons
-          answers.forEach((button)=>{
-            button.removeEventListener("click", tryGuess, false);
-          })
+        QuestionsCtrl.clearAnswers();  // empty string of answers
+        QuestionsCtrl.hideAnswers();   // hide block 'answers'
 
-          let actorId = actor.id;
-          // store actor's id into LS
-          LocalStorageCtrl.setPersonIdToLS(actorId);  // mark an actor as completed, put his/her id to local storage to filter it out at new game
+        LevelCtrl.IncreaseScoreByOne(); // incement score 
 
-          document.querySelector('.welcome-text').textContent = "Proceed to next question"; // change text before level 2 start
+        // remove event listeners from buttons
+        // answers.forEach((button)=>{
+        //   button.removeEventListener("click", tryGuess, false);
+        // })
+        let actor = PersonCtrl.getPerson();
+        let actorId = actor.id;
+        // store actor's id into LS
+        LocalStorageCtrl.setPersonIdToLS(actorId);  // mark an actor as completed, put his/her id to local storage to filter it out at new game
 
-          LevelCtrl.updateLevel();  // update level by 1
+        document.querySelector('.welcome-text').textContent = "Proceed to next question"; // change text before level 2 start
 
-          LevelCtrl.initText();     // go to next level
+        LevelCtrl.updateLevel();  // update level by 1
 
-        } else {
-          console.log('bad answer! Game over!');
-          //game over
-        }
+        LevelCtrl.initText();     // go to next level
 
+      } else {
+        console.log('bad answer! Game over!');
+        //game over
       }
 
     },
